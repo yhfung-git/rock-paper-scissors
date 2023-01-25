4c5af26bba324795e786a0af37759e67a3c0a531
yonghowfung <yhfung940217@gmail.com>
2023-01-25 23:52:41 +0100

    js computer choice and player choice set up and playgame recevied values of them

--- Javascript/game.js
+++ Javascript/game.js
@@ -1,18 +1,36 @@
-// Game
-let playerScore = 0
-let computerScore = 0
+let playerScore = 0;
+let computerScore = 0;
+
+const userRock = document.getElementById("user-rock");
+const userPaper = document.getElementById("user-paper");
+const userScissors = document.getElementById("user-scissors");
+
+const computerRock = document.getElementById("computer-rock");
+const computerPaper = document.getElementById("computer-paper");
+const computerScissors = document.getElementById("computer-scissors");
+
+const choices = ["computer-rock", "computer-paper", "computer-scissors"];
+const buttons = document.querySelectorAll(".user-buttons button")
 
 // Computer choice, return rock, paper or scissors randomly
-const choices = ["rock", "paper", "scissors"]
-
 function getComputerChoice() {
   return choices[Math.floor(Math.random() * choices.length
-  )]
+  )];
+};
+
+// Player choice
+buttons.forEach(button => {
+  button.addEventListener("click", function() {
+    playGame(button.id)
+  });
+});
+
+
+// Play game
+function playGame(playerSelection) {
+  const computerChoice = getComputerChoice();
+  const playerChoice = playerSelection;
+
+  // console.log(computerChoice);
+  // console.log(playerChoice);
 }
-console.log(getComputerChoice());
-
-// Get player choice
-function playerSelection() {
-
-}
-
